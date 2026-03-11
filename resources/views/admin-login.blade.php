fc6d5561df88d759ef076ee8b3d7d3bedbb21db1
Saddam Khoso <saddamkhoso203@gmail.com>
2026-03-12 04:49:24 +0500

    admin-login design completed

---
+++ resources/views/admin-login.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"> 
+ 
+    <title>Admin Login</title>
+    @vite('resources/css/app.css')
+</head>
+<body class="bg-gray-100 flex items-center justify-center min-h-screen">
+  <dev class="bg-white p-8 rounded-2xl shadow-lg w-full max-w-sm">
+  <h1 class="text-2x1 text-center text-gray-800 mb-6">Admin Login</h1>
+  <form action="" method="post" class= "space-y-4">
+   <div>
+    <label for="" class="text-gray-600 mb-1">Admin Name</label>
+    <input type="text"placeholder="Enter Admin Name"
+    
+    class="w-full px-4 py-2 border border-gray-300 rounded-xl focus: outline-none"
+    >
+   </div>
+      <div>
+    <label for="" class="text-gray-600 mb-1">Password</label>
+    <input type="text"placeholder="Enter Admin Password"
+    class="w-full px-4 py-2 border border-gray-300 rounded-xl focus: outline-none">
+   </div>
+   <button type="submit" class="w-full bg-blue-500 rounded-xl px-4 py-2 text-white">Login</button>
+  </form>
+
+  </dev>
+</body>
+</html>
