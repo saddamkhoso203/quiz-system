6f3a3ffa0495ea626ad5d87ea765bc8d46abcbe3
Saddam Khoso <saddamkhoso203@gmail.com>
2026-04-24 04:16:41 +0500

    working on validation

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,11 +9,33 @@ class AdminController extends Controller
     //admin login
     function login(Request $request){
 
+//for validation
+
+$validation = $request -> validate([
+
+"name"=> "required",
+"password"=>"required"
+
+]);
+
+
     $admin = Admin::where([
   ['name',"=",$request->name],
   ['password',"=",$request->password],
 
     ])->first();
+if (!$admin) {
+ $validation = $request -> validate([
+
+"user"=> "required",
+
+
+],[
+  "user.required"=> "user is not found"
+]);
+}
+
+
     return view('admin-view', ["name"=>$admin->name]);
     }
 }
